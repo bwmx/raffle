a903f2cfede6d9c4e5144bed18138f75294f227d
bwmx <blairmcilroy@icloud.com>
2023-12-14 15:32:04 +0000

    Project initialised with AlgoKit CLI using template: https://github.com/algorand-devrel/tealscript-algokit-template.git

---
+++ __test__/raffle.test.ts
@@ -0,0 +1,41 @@
+import { describe, test, expect, beforeAll, beforeEach } from '@jest/globals';
+import { algorandFixture } from '@algorandfoundation/algokit-utils/testing';
+import { RaffleClient } from '../contracts/clients/RaffleClient';
+
+const fixture = algorandFixture();
+
+let appClient: RaffleClient;
+
+describe('Raffle', () => {
+  beforeEach(fixture.beforeEach);
+
+  beforeAll(async () => {
+    await fixture.beforeEach();
+    const { algod, testAccount } = fixture.context;
+
+    appClient = new RaffleClient(
+      {
+        sender: testAccount,
+        resolveBy: 'id',
+        id: 0,
+      },
+      algod
+    );
+
+    await appClient.create.createApplication({});
+  });
+
+  test('sum', async () => {
+    const a = 13;
+    const b = 37;
+    const sum = await appClient.doMath({ a, b, operation: 'sum' });
+    expect(sum.return?.valueOf()).toBe(BigInt(a + b));
+  });
+
+  test('difference', async () => {
+    const a = 13;
+    const b = 37;
+    const diff = await appClient.doMath({ a, b, operation: 'difference' });
+    expect(diff.return?.valueOf()).toBe(BigInt(a >= b ? a - b : b - a));
+  });
+});
