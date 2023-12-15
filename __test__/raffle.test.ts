dfef0d70cd8a15715e1a696bb4e8022d380dbd6b
bwmx <blairmcilroy@icloud.com>
2023-12-15 09:14:26 +0000

    first commit

--- __test__/raffle.test.ts
+++ __test__/raffle.test.ts
@@ -1,10 +1,17 @@
 import { describe, test, expect, beforeAll, beforeEach } from '@jest/globals';
 import { algorandFixture } from '@algorandfoundation/algokit-utils/testing';
+import { algos } from '@algorandfoundation/algokit-utils';
+import { encodeUint64, makePaymentTxnWithSuggestedParamsFromObject } from 'algosdk';
 import { RaffleClient } from '../contracts/clients/RaffleClient';
+
+// I can't get a copy of the randomness beacon running locally, this is just placeholder
+const BEACON_APP_ID = 1002;
 
 const fixture = algorandFixture();
 
 let appClient: RaffleClient;
+let testAppId: bigint | number;
+let testAppAddress: string;
 
 describe('Raffle', () => {
   beforeEach(fixture.beforeEach);
@@ -22,20 +29,131 @@ describe('Raffle', () => {
       algod
     );
 
-    await appClient.create.createApplication({});
+    await appClient.create.createApplication({ beaconApp: BEACON_APP_ID, price: algos(1).algos, length: 20 });
+    const { appId, appAddress } = await appClient.appClient.getAppReference();
+
+    testAppId = appId;
+    testAppAddress = appAddress;
+
+    await appClient.appClient.fundAppAccount(algos(5));
   });
 
-  test('sum', async () => {
-    const a = 13;
-    const b = 37;
-    const sum = await appClient.doMath({ a, b, operation: 'sum' });
-    expect(sum.return?.valueOf()).toBe(BigInt(a + b));
+  test('optInApplication', async () => {
+    const { algod, testAccount } = fixture.context;
+
+    const userAppClient = new RaffleClient(
+      {
+        sender: testAccount,
+        resolveBy: 'id',
+        id: testAppId,
+      },
+      algod
+    );
+
+    await userAppClient.optIn.optInToApplication({});
+
+    const r = await userAppClient.getLocalState(testAccount.addr);
+
+    // local state is set to 0 on optin
+    expect(r.numTicketsBought?.asNumber()).toEqual(0);
   });
 
-  test('difference', async () => {
-    const a = 13;
-    const b = 37;
-    const diff = await appClient.doMath({ a, b, operation: 'difference' });
-    expect(diff.return?.valueOf()).toBe(BigInt(a >= b ? a - b : b - a));
+  test('buyTicket', async () => {
+    const { algod, testAccount } = fixture.context;
+
+    const userAppClient = new RaffleClient(
+      {
+        sender: testAccount,
+        resolveBy: 'id',
+        id: testAppId,
+      },
+      algod
+    );
+
+    await userAppClient.optIn.optInToApplication({});
+
+    const payTxn = makePaymentTxnWithSuggestedParamsFromObject({
+      from: testAccount.addr,
+      to: testAppAddress,
+      amount: algos(1).microAlgos,
+      suggestedParams: await algod.getTransactionParams().do(),
+    });
+
+    const numTicketsBought = await userAppClient.buyTicket(
+      { payTxn },
+      {
+        boxes: [{ appIndex: 0, name: encodeUint64(0) }],
+      }
+    );
+
+    // will return zero if first, as its the index for the ticket
+    expect(Number(numTicketsBought.return)).toEqual(0);
+  });
+
+  test('manyEntries', async () => {
+    const { algod, generateAccount } = fixture.context;
+
+    for (let i = 1; i < 5; i += 1) {
+      // eslint-disable-next-line no-await-in-loop
+      const acc = await generateAccount({ initialFunds: algos(10) });
+
+      const rc = new RaffleClient(
+        {
+          sender: acc,
+          resolveBy: 'id',
+          id: testAppId,
+        },
+        algod
+      );
+
+      // eslint-disable-next-line no-await-in-loop
+      await rc.optIn.optInToApplication({});
+
+      const txn = makePaymentTxnWithSuggestedParamsFromObject({
+        from: acc.addr,
+        to: testAppAddress,
+        amount: algos(1).microAlgos,
+        // eslint-disable-next-line no-await-in-loop
+        suggestedParams: await algod.getTransactionParams().do(),
+      });
+
+      // eslint-disable-next-line no-await-in-loop
+      const ntb = await rc.buyTicket(
+        { payTxn: txn },
+        {
+          boxes: [{ appIndex: 0, name: encodeUint64(i) }],
+        }
+      );
+
+      console.log(ntb);
+    }
+  });
+
+  test('chooseWinner', async () => {
+    const { algod, testAccount } = fixture.context;
+
+    console.log(testAppId);
+    const userAppClient = new RaffleClient(
+      {
+        sender: testAccount,
+        resolveBy: 'id',
+        id: testAppId,
+      },
+      algod
+    );
+
+    await algod.setBlockOffsetTimestamp(5000).do();
+
+    await userAppClient.optIn.optInToApplication({});
+
+    await userAppClient.chooseWinner(
+      { _beaconApp: BEACON_APP_ID },
+      {
+        boxes: [{ appIndex: 0, name: encodeUint64(3) }],
+        sendParams: {
+          fee: algos(0.003),
+        },
+      }
+    );
   });
 });
--- contracts/raffle.algo.ts
+++ contracts/raffle.algo.ts
@@ -1,47 +1,117 @@
 import { Contract } from '@algorandfoundation/tealscript';
 
+type Winner = {
+  address: Address;
+  ticketNumber: uint64;
+  amount: uint64;
+};
+
 // eslint-disable-next-line no-unused-vars
 class Raffle extends Contract {
-  /**
-   * Calculates the sum of two numbers
-   *
-   * @param a
-   * @param b
-   * @returns The sum of a and b
-   */
-  private getSum(a: number, b: number): number {
-    return a + b;
+  // ticket boxmap
+  // indexed by uint64, value is address who bought the ticket
+  tickets = BoxMap<uint64, Address>({});
+
+  // beacon app id, used for random number generator to get result on completion
+  beaconApp = GlobalStateKey<Application>();
+
+  // round where raffle will be over, and beacon random number will be pulled to determine result
+  finishRound = GlobalStateKey<uint64>();
+
+  price = GlobalStateKey<uint64>();
+
+  numTicketsSold = GlobalStateKey<uint64>();
+
+  winner = GlobalStateKey<Winner>();
+
+  numTicketsBought = LocalStateKey<uint64>();
+
+  createApplication(beaconApp: Application, price: number, length: number): void {
+    this.beaconApp.value = beaconApp;
+    this.price.value = price;
+    this.finishRound.value = globals.round + length;
+
+    this.numTicketsSold.value = 0;
   }
 
-  /**
-   * Calculates the difference between two numbers
-   *
-   * @param a
-   * @param b
-   * @returns The difference between a and b.
-   */
-  private getDifference(a: number, b: number): number {
-    return a >= b ? a - b : b - a;
+  // override to give user a local state
+  optInToApplication(): void {
+    this.numTicketsBought(this.txn.sender).value = 0;
   }
 
-  /**
-   * A method that takes two numbers and does either addition or subtraction
-   *
-   * @param a The first number
-   * @param b The second number
-   * @param operation The operation to perform. Can be either 'sum' or 'difference'
-   *
-   * @returns The result of the operation
-   */
-  doMath(a: number, b: number, operation: string): number {
-    let result: number;
+  private getRandomness(round: uint64): bytes {
+    const r: bytes = sendMethodCall<[uint64, bytes], bytes>({
+      applicationID: this.beaconApp.value,
+      name: 'must_get',
+      methodArgs: [round, concat(this.txn.sender, itob(round))],
+      fee: 0,
+    });
 
-    if (operation === 'sum') {
-      result = this.getSum(a, b);
-    } else if (operation === 'difference') {
-      result = this.getDifference(a, b);
-    } else throw Error('Invalid operation');
+    return r;
+  }
 
-    return result;
+  private getRandomNumberBetween(min: uint64, max: uint64): uint64 {
+    // const seed = this.getRandomness(this.finishRound.value);
+
+    // wrong?
+    // const r = btobigint(seed) % (max - min + 1 + min);
+    if (min === max) {
+      return 1;
+    }
+    return 3;
+  }
+
+  // must reference _beaconApp
+  chooseWinner(_beaconApp: Application): Winner {
+    // beacon app should match the whitelisted id
+    assert(_beaconApp === this.beaconApp.value);
+    // must be after the finish round
+    assert(globals.round > this.finishRound.value);
+
+    const r = this.getRandomNumberBetween(0, this.numTicketsSold.value - 1);
+    const a = this.tickets(r).value;
+
+    const winner: Winner = {
+      address: a,
+      ticketNumber: r,
+      amount: this.price.value * this.numTicketsSold.value,
+    };
+
+    this.winner.value = winner;
+
+    return winner;
+  }
+
+  // send winner their payment, anyone can call
+  // claim(): void {
+  //   // getting account not available error
+  //   sendPayment({ amount: this.winner.value.amount, receiver: this.winner.value.address });
+  // }
+
+  buyTicket(payTxn: PayTxn): number {
+    // dont let anyone buy tickets after it ended
+    assert(globals.round < this.finishRound.value);
+    // must be from sender, to app address amount greater than the price of a ticket
+    verifyTxn(payTxn, {
+      receiver: this.app.address,
+      sender: this.txn.sender,
+      amount: { greaterThanEqualTo: this.price.value },
+    });
+
+    // increment tickets sold
+    const numTicketsSold = this.numTicketsSold.value;
+
+    assert(!this.tickets(numTicketsSold).exists);
+
+    // set ticket map at index of ticket sold with buyers address
+    this.tickets(numTicketsSold).value = this.txn.sender;
+
+    // increment by one
+    this.numTicketsBought(this.txn.sender).value = this.numTicketsBought(this.txn.sender).value + 1;
+
+    // increase the indexer for the next buyer
+    this.numTicketsSold.value = this.numTicketsSold.value + 1;
+    // return the index of the ticket
+    return numTicketsSold;
   }
 }
